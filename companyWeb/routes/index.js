b3c7b9d67ea9017639fd87051ac6940b23203034
sky <291842650@qq.com>
2018-04-15 21:31:08 +0800

    415

--- companyWeb/routes/index.js
+++ companyWeb/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '企业网站搭建-云工作室' ,"b":false});
 });
-
+router.get('/banners', function(req, res, next) {
+  res.render('index2', { title: '咕噜噜/banners' });
+});
 module.exports = router;
