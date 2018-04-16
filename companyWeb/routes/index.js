4496c2e69457878d1de1b71b8f4854c18e86bb39
sky <291842650@qq.com>
2018-04-16 20:59:42 +0800

    416

--- companyWeb/routes/index.js
+++ companyWeb/routes/index.js
@@ -7,23 +7,20 @@ var router = express.Router();
 router.get('/', function(req, res, next){
 
 var selectSQL = 'select * from n_user limit 10';
-conf.query(selectSQL,function(err,result){
-  // console.log(res);
-  var string=JSON.stringify(result);
-          // console.log(string);
-          //将json字符串转化成json数组
-          var json=JSON.parse(string);
-          // console.log(json);
-       if(err){
-          data= '';
-       }else {
-           data= json;
-       }
-       // console.log(json2);
-       res.render('index', { title: "" ,"b":false,user:data});//企业网站搭建-云工作室
-   });
- // console.log(q);
-
+//服务器未配置数据库
+// conf.query(selectSQL,function(err,result){
+//     var string=JSON.stringify(result);
+//           //将json字符串转化成json数组
+//           var json=JSON.parse(string);
+//           // console.log(json);
+//        if(err){
+//           data= '';
+//        }else {
+//            data= json;
+//        }
+//        res.render('index', { title: "" ,"b":false,user:data});//企业网站搭建-云工作室
+//    });
+res.render('index', { title: "" ,"b":false,user:data});//企业网站搭建-云工作室
 });
 router.get('/banners', function(req, res, next) {
   res.render('index2', { title: '咕噜噜/banners' });
