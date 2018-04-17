2f3aa3cda50ae93137ed0a9592ffc4ecb476e31d
sky <291842650@qq.com>
2018-04-17 23:48:07 +0800

    417

---
+++ companyWeb/public/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+      // var paper = Raphael(100, 150, 120, 100);
+      // var rect = paper.rect(50, 50, 50, 50);
+      // rect.attr("fill", "blue");
+      // rect.attr("stroke", "#fff");
+
+      // rect.translate(50,50);
+
+
+}
--- companyWeb/routes/details.js
+++ companyWeb/routes/details.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/a', function(req, res, next) {
-  res.send('respond with a resourceaa');
+router.get('/d', function(req, res, next) {
+  res.send('details');
 });
 
 module.exports = router;
