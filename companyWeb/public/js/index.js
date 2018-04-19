b9e5fdb780150b12648ba625681c0f098794e475
sky <291842650@qq.com>
2018-04-20 00:00:45 +0800

    420

--- companyWeb/public/js/index.js
+++ companyWeb/public/js/index.js
@@ -1,11 +1,21 @@
 window.onload = function(){
+    var Paper = Raphael('pos', 1000, 130);
+    var p = Paper.image('../images/pos.jpg',0, 0, 120, 120);
 
-      // var paper = Raphael(100, 150, 120, 100);
+    // p.attr('height',180);
+    p.animate({"transform":"t800,100r65t-100,10 r205t"},1000,'bounce',function(r){
+
+    });
+    //     var circle = paper.circle(10, 50, 10)
+    //     circle.attr({"fill":"yellow"})
+    //     circle.animate({cx: 10, cy: 20, r: 8, "fill": "blue"},1000,function(){
+    //     });
       // var rect = paper.rect(50, 50, 50, 50);
       // rect.attr("fill", "blue");
       // rect.attr("stroke", "#fff");
 
       // rect.translate(50,50);
+
 
 
 }
